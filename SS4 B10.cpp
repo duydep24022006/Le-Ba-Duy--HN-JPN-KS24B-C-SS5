ed7c663f365e742f1991e9ac5057c8a7238a7b11
duydep24022006 <memay26032006@gmail.com>
2024-11-30 01:51:43 +0700

    Add files via upload

---
+++ SS4 B10.cpp
@@ -0,0 +1,13 @@
+#include <stdio.h> 
+
+int main(){
+	int n;
+	printf("moi ban nhap so muon in ra tat ca tru so trong so do ");
+	scanf("%d",&n); 
+	for(int i=0;i<=n;i++){
+		printf("%d ",i);
+	} 
+	return 0; 
+} 
+
+ 
